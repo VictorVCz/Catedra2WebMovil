b72d4141881d89dcc443ef85aa0b61d763f03fbc
VictorVCz <vvelcruz@gmail.com>
2023-11-22 14:41:26 -0300

    Commit inicial

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Descrition { get; set; }
+        
+    }
+}
